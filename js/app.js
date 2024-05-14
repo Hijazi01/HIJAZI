7cb667204fba235f2e0506375dcea975852ca73f
Ahmed Mostafa Hijazi <103125286+Hijazi01@users.noreply.github.com>
2024-05-14 21:40:58 +0300

    Upload ALL Data

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var Myimg = document.querySelector(".my-photo");
+var Myim = document.querySelector(".my-photo");
+var Myi = document.querySelector(".my-photo");
+
+Myimg.addEventListener('contextmenu',function(e){
+    e.preventDefault()
+    console.log("clicked right")
+});
+
